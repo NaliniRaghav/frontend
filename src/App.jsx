9acd83f41d53df729633dd1ac7a28d3aa8e4c3d8
Nalini Raghavendra <naliniraghavendra@nalinis-new-mbp.lan>
2024-10-17 11:56:07 -0400

    third

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import ProjectDetails from "./pages/ProjectDetails";
 import "./App.css";
+console.log(import.meta.env.VITE_API_BASE_URL)
 
 function App() {
   const [projects, setProjects] = useState([]);
@@ -13,7 +14,8 @@ function App() {
   useEffect(() => {
     // fetch all projects
     const fetchProjects = async () => {
-      const res = await fetch("http://localhost:4000/api/projects/");
+      // const res = await fetch("http://localhost:4000/api/projects/");
+      const res = await fetch(`${import.meta.env.VITE_API_BASE_URL}/api/projects/`);
       const projectsData = await res.json();
       console.log(projectsData);
       setProjects(projectsData.projects);
